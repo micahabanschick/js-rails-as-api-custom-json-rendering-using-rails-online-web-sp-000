78601a798671b86cf2adac6087d3f677cd7f2a19
Micah Banschick <micha.ban@gmail.com>
2021-03-11 17:59:26 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,11 +1,7 @@
 class BirdsController < ApplicationController
   def index
     birds = Bird.all
-<<<<<<< HEAD
     render json: birds, only: [:id, :name, :species]
-=======
-    render json: birds
->>>>>>> 631a43fbc8b27c97103df8c99ea07ef0c80790a6
   end
  
   def show
